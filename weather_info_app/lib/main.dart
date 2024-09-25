947aeed5a7715c39f5d0103cc56cd66e717a62a6
jd <joisedivya@Joises-MacBook-Air.local>
2024-09-25 19:15:57 -0400

    Initial project setup and structure

--- weather_info_app/lib/main.dart
+++ weather_info_app/lib/main.dart
@@ -8,9 +8,14 @@ class WeatherInfoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Weather Info',
       home: Scaffold(
-        appBar: AppBar(title: Text('Weather Info App')),
-        body: Center(child: Text('Weather Information will be displayed here.')),
+        appBar: AppBar(
+          title: Text('Weather Info'),
+        ),
+        body: Center(
+          child: Text('Weather Info App'),
+        ),
       ),
     );
   }
