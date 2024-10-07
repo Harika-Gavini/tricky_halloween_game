982fdfa6d2cb43e2ce3b678c190b853a844bdddc
divya-jd <gorla.joise@gmail.com>
2024-10-07 19:00:15 -0400

    added background image, and other halloween related theme images and animationsanimated objects related to halloween

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,197 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(HalloweenGame());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class HalloweenGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Halloween Game',
+      theme: ThemeData.dark(),
+      home: HalloweenHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HalloweenHomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HalloweenHomePageState createState() => _HalloweenHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HalloweenHomePageState extends State<HalloweenHomePage> {
+  final Random random = Random();
+  List<Offset> objectPositions = [];
+  List<Offset> objectDirections = [];
+  String correctItem = "";
+  bool showSuccess = false;
+  bool showFailure = false;
+  late Timer _timer;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _initializeObjects();
+    _selectCorrectItem();
+    _startMovement();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  // Initialize random positions and directions for each object
+  void _initializeObjects() {
+    objectPositions = List.generate(5, (index) {
+      return Offset(random.nextDouble() * 300, random.nextDouble() * 500);
     });
+
+    // Give each object a random direction to move in
+    objectDirections = List.generate(5, (index) {
+      double dx = random.nextBool() ? 1.0 : -1.0;
+      double dy = random.nextBool() ? 1.0 : -1.0;
+      return Offset(dx, dy);
+    });
+  }
+
+  void _selectCorrectItem() {
+    // Define possible spooky objects
+    final spookyItems = ['ghost', 'pumpkin', 'bat'];
+    // Randomly select one as the correct item
+    correctItem = spookyItems[random.nextInt(spookyItems.length)];
+  }
+
+  void _startMovement() {
+    _timer = Timer.periodic(Duration(milliseconds: 50), (timer) {
+      setState(() {
+        // Update the position of each object
+        for (int i = 0; i < objectPositions.length; i++) {
+          Offset newPos = objectPositions[i] + objectDirections[i] * 3;
+
+          // Make sure objects stay within screen bounds
+          if (newPos.dx < 0 || newPos.dx > MediaQuery.of(context).size.width - 80) {
+            objectDirections[i] = Offset(-objectDirections[i].dx, objectDirections[i].dy);
+          }
+          if (newPos.dy < 0 || newPos.dy > MediaQuery.of(context).size.height - 80) {
+            objectDirections[i] = Offset(objectDirections[i].dx, -objectDirections[i].dy);
+          }
+
+          objectPositions[i] = objectPositions[i] + objectDirections[i] * 3;
+        }
+      });
+    });
+  }
+
+  // Function to handle tap on the object
+  void _handleItemTap(String tappedItem) {
+    if (tappedItem == correctItem) {
+      setState(() {
+        showSuccess = true;
+        showFailure = false;
+      });
+    } else {
+      setState(() {
+        showFailure = true;
+        showSuccess = false;
+      });
+      Future.delayed(Duration(seconds: 1), () {
+        setState(() {
+          showFailure = false;
+        });
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: Stack(
+        children: [
+          // Background Image
+          Positioned.fill(
+            child: Image.asset(
+              '/Users/joisedivya/halloween_game/assets/dark_background.png', // Make sure to use a dark, spooky background image.
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          // Animated Spooky Objects
+          ..._buildMovingSpookyObjects(),
+
+          // Success Message
+          if (showSuccess) _buildSuccessMessage(),
+
+          // Failure Message
+          if (showFailure) _buildFailureMessage(),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    );
+  }
+
+  // Function to build the moving spooky objects
+  List<Widget> _buildMovingSpookyObjects() {
+    final imageAssets = {
+      'ghost': '/Users/joisedivya/halloween_game/assets/ghost.png', // Make sure these images are transparent PNGs
+      'pumpkin': '/Users/joisedivya/halloween_game/assets/pumpkin.png',
+      'bat': '/Users/joisedivya/halloween_game/assets/bat.png'
+    };
+
+    final itemTypes = imageAssets.keys.toList();
+
+    return List<Widget>.generate(objectPositions.length, (index) {
+      final itemType = itemTypes[random.nextInt(itemTypes.length)];
+      final image = imageAssets[itemType]!;
+
+      return Positioned(
+        left: objectPositions[index].dx,
+        top: objectPositions[index].dy,
+        child: GestureDetector(
+          onTap: () {
+            _handleItemTap(itemType);
+          },
+          child: Image.asset(
+            image,
+            width: 80,
+            height: 80,
+          ),
+        ),
+      );
+    });
+  }
+
+  // Success message with glowing animation
+  Widget _buildSuccessMessage() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'You found the correct item!',
+            style: TextStyle(fontSize: 30, color: Colors.green, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          Icon(Icons.star, size: 100, color: Colors.yellow),
+        ],
+      ),
+    );
+  }
+
+  // Spooky failure reaction with animation
+  Widget _buildFailureMessage() {
+    return Container(
+      color: Colors.red.withOpacity(0.5), // Flash red color
+      child: Center(
+        child: Text(
+          'Wrong Item! Try Again!',
+          style: TextStyle(fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
